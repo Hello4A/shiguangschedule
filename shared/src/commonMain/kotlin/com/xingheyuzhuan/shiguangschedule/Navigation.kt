57312d7c56dca6d55f684bb325915ae349b0d43d
Hello4A <149310598+Hello4A@users.noreply.github.com>
2026-09-09 02:48:12 +0800

    feat(qzh5): add auto-sync settings destination

--- shared/src/commonMain/kotlin/com/xingheyuzhuan/shiguangschedule/Navigation.kt
+++ shared/src/commonMain/kotlin/com/xingheyuzhuan/shiguangschedule/Navigation.kt
@@ -48,6 +48,7 @@ sealed interface Destination : NavKey {
     @Serializable data object ThemeSettings : Destination
     @Serializable data object BackupAndRestore : Destination
     @Serializable data object LanguageSettings : Destination
+    @Serializable data object Qzh5AutoSyncSettings : Destination
 
     // --- 动态传参页面 ---
     @Serializable
@@ -106,6 +107,7 @@ val navSerializersModule = SerializersModule {
         subclass(Destination.ThemeSettings::class)
         subclass(Destination.BackupAndRestore::class)
         subclass(Destination.LanguageSettings::class)
+        subclass(Destination.Qzh5AutoSyncSettings::class)
 
         // 带参数据类
         subclass(Destination.AdapterSelection::class)
